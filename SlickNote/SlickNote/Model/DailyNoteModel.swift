0a12c58f015bd1d0cd69782d73441c3182f815a2
Davy <dchuon@ucdavis.edu>
2023-06-22 14:43:28 -0700

    created DailyNote model

---
+++ SlickNote/SlickNote/Model/DailyNoteModel.swift
@@ -0,0 +1,15 @@
+//
+//  DailyNoteModel.swift
+//  SlickNote
+//
+//  Created by Davy Chuon on 6/22/23.
+//
+
+import Foundation
+
+struct DailyNote {
+    var note: String
+    var pinned: Bool
+    var checked: Bool
+    var index: Int
+}
