e645b9091ae5c2c1ae6a5cff5a479f3f37499a64
Davy <dchuon@ucdavis.edu>
2023-06-28 12:17:20 -0700

    created CoreDataManager class to abstract out core data logic from VC

---
+++ SlickNote/SlickNote/Model/CoreDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  CoreDataManager.swift
+//  SlickNote
+//
+//  Created by Davy Chuon on 6/28/23.
+//
+
+import CoreData
+import UIKit
+
+class CoreDataManager {
+    private let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+}
